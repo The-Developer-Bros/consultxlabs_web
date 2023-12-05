0b6d495e2eacca4bf60538c5cc541f6d4d55c345
Kaustav Ghosh <teetangh@gmail.com>
2023-12-05 10:04:41 +0530

    moved firebase config to seperate file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { firestore } from "@/app/firebase.config";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { profile } from "console";
 import { getApp, getApps, initializeApp } from "firebase/app";
@@ -23,19 +24,6 @@ import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-const firebaseConfig: Record<string, string> = {
-  apiKey: process.env.FIREBASE_API_KEY!,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.FIREBASE_APP_ID!,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
-};
-
-const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig); // This is to ensure that we don't initialize the app more than once
-const firestore = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
 
 const authOptions: AuthOptions = {
   providers: [
@@ -130,8 +118,5 @@ const authHandler = NextAuth(authOptions);
 export {
   authHandler as GET,
   authHandler as POST,
-  auth,
   authOptions,
-  firebaseApp,
-  firestore,
 };
