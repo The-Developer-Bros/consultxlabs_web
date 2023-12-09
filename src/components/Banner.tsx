95e6007780d1ce4bf1d13f1256da5255cf7a1c88
Kaustav Ghosh <teetangh@gmail.com>
2023-12-09 18:11:14 +0530

    Update Tailwind configuration and banner. Made Navbar responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -29,41 +29,41 @@ const Banner = () => {
       >
         The One Stop Platform for all your queries
       </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -800%)",
-          textAlign: "center",
-          color: "rgb(43, 43, 43)",
-          fontSize: "24px",
-          fontWeight: "600",
-          lineHeight: "32px",
-          fontFamily: "__geistSansFont_8289af, arial",
-          width: "60%",
-        }}
-      >
-        Get the best advice from our experts. From career to relationships, we
-        have got you covered.
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "60%",
-          left: "50%",
-          transform: "translate(-50%, -1200%)",
-        }}
-      >
-        <a
-          href="/link-to-page"
-          className="bg-gradient-to-r from-black to-slate-700 px-6 py-3 text-white rounded-full"
-        >
-          Look for Experts -&gt;
-        </a>
-      </div>
-    </div>
-  );
-};
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -800%)",
+                textAlign: "center",
+                color: "rgb(43, 43, 43)",
+                fontSize: "24px",
+                fontWeight: "600",
+                lineHeight: "32px",
+                fontFamily: "__geistSansFont_8289af, arial",
+                width: "60%",
+              }}
+            >
+              Get the best advice from our experts. From career to relationships, we
+              have got you covered.
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                top: "60%",
+                left: "50%",
+                transform: "translate(-50%, -1200%)",
+              }}
+            >
+              <a
+                href="/explore/experts"
+                className="bg-gradient-to-r from-black to-slate-700 px-8 py-4 text-white rounded-full"
+              >
+                Look for Experts -&gt;
+              </a>
+            </div>
+          </div>
+        );
+      };
 
-export default Banner;
+      export default Banner;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,102 +1,113 @@
-import { RootState } from "@/redux/store";
-import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useSelector } from "react-redux";
-
-import { firebaseAuth } from "@/app/firebase.config";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useSession, signOut } from "next-auth/react";
+import { RootState } from "@/redux/store";
 import consultxlogo from "../../public/static/assets/logos/ConsultX-logos/ConsultX-logos_transparent.png";
 
 const Navbar = () => {
+  const router = useRouter();
   const isAnnouncementBarOpen = useSelector(
     (state: RootState) => state.announcement.isAnnouncementBarOpen
   );
-
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const router = useRouter();
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <nav
-      style={{
-        position: "fixed",
-        top: isAnnouncementBarOpen ? "40px" : "0",
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "1em",
-        background: "white",
-        alignItems: "center",
-        zIndex: 2,
-      }}
-    >
-      <Image src={consultxlogo} alt="ConsultX Logo" height={60} />
+    <>
+      {/* Main Navbar */}
+      <nav
+        className={`fixed top-0 w-full z-50 bg-white ${
+          isAnnouncementBarOpen ? "pt-12" : "pt-0"
+        } px-6 lg:px-0`}
+      >
+        <div className="flex justify-between items-center">
+          <Image src={consultxlogo} alt="ConsultX Logo" height={60} />
 
-      <div style={{ display: "flex", alignItems: "center", gap: "50px" }}>
-        {session?.user && <button>Feed</button>}
-        <button style={{ color: "black" }}>Experts</button>
-        <button style={{ color: "black" }}>Webinar</button>
-        <button style={{ color: "black" }}>Events</button>
-        <button style={{ color: "black" }}>Community</button>
-        <button style={{ color: "black" }}>Blog</button>
-      </div>
+          <div className="lg:hidden">
+            <button onClick={toggleMenu} aria-label="Toggle Navigation">
+              ☰
+            </button>
+          </div>
 
-      <div style={{ display: "flex", alignItems: "center" }}>
-        {session?.user ? (
-          <>
-            <Image
-              src={session?.user ? session.user.image! : ""}
-              alt="Profile"
-              width={50}
-              height={10}
-              className="rounded-full cursor-pointer"
-            />
-            <button
-              onClick={() => {
-                signOut();
-                // TODO: Work on this
-                // firebaseAuth.signOut(); 
-              }}
-              style={{ marginLeft: "10px", color: "black" }}
-            >
-              Sign out
-            </button>
-          </>
-        ) : (
-          <>
-            <button
-              style={{
-                marginRight: "10px",
-                color: "black",
-                border: "1px solid black",
-                borderRadius: "5px",
-                padding: "5px 10px",
-              }}
-              onClick={() => {
-                /* Implement your sign up logic here */
-              }}
-            >
-              Sign up
-            </button>
-            <button
-              style={{
-                marginRight: "10px",
-                color: "white",
-                background: "black",
-                border: "1px solid black",
-                borderRadius: "5px",
-                padding: "5px 10px",
-              }}
-              onClick={() => {
-                router.replace("/auth/signin");
-              }}
-            >
-              Sign in
-            </button>
-          </>
-        )}
-      </div>
-    </nav>
+          <div className="hidden lg:flex gap-8">
+            {session?.user && <button>Feed</button>}
+            <button>Experts</button>
+            <button>Webinar</button>
+            <button>Events</button>
+            <button>Community</button>
+            <button>Blog</button>
+          </div>
+
+          <div className="flex items-center">
+            {session?.user ? (
+              <>
+                <Image
+                  src={session.user.image!}
+                  alt="Profile"
+                  width={50}
+                  height={50}
+                  className="rounded-full cursor-pointer"
+                />
+                <button
+                  onClick={() => {
+                    signOut();
+                  }}
+                  className="ml-2"
+                >
+                  Sign out
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  className="mr-2 border border-black rounded px-2 py-1"
+                  onClick={() => {
+                    /* Implement your sign-up logic here */
+                  }}
+                >
+                  Sign up
+                </button>
+                <button
+                  className="mr-2 bg-black text-white border border-black rounded px-2 py-1"
+                  onClick={() => {
+                    router.replace("/auth/signin");
+                  }}
+                >
+                  Sign in
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
+
+      {/* Side Drawer */}
+      {isOpen && (
+        <div className="lg:hidden fixed top-0 left-0 h-full w-4/5 bg-white z-50 transition-transform duration-500 ease-in-out transform translate-x-0">
+          <div className="flex justify-end p-4">
+            <button onClick={closeMenu}>&times;</button>
+          </div>
+          <div className="flex flex-col gap-4 p-4">
+            {session?.user && <button>Feed</button>}
+            <button>Experts</button>
+            <button>Webinar</button>
+            <button>Events</button>
+            <button>Community</button>
+            <button>Blog</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
