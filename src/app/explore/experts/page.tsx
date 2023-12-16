925bb602df24d942bb03853e5e0b77ec59cd5202
Kaustav Ghosh <teetangh@gmail.com>
2023-12-16 13:40:29 +0530

    Add new Radix UI components and update page content

--- src/app/explore/experts/page.tsx
+++ src/app/explore/experts/page.tsx
@@ -47,20 +47,20 @@ export default function ExploreExperts() {
           <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
             <div>
               <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                The Best Keyboards for You
+                The Best Experts in the World
               </h1>
             </div>
             <div className="flex flex-col items-start space-y-4">
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                Explore our wide range of keyboards. Perfect for gaming, work,
-                and everything in between.
+                Explore our wide range of consultants and find the right one for
+                your business.
               </p>
               <div className="space-x-4">
                 <Link
                   className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                   href="#"
                 >
-                  Shop Now
+                  Get Started
                 </Link>
               </div>
             </div>
@@ -79,45 +79,45 @@ export default function ExploreExperts() {
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
               <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-                Featured Keyboards
+                Featured Experts
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Top Picks for You
+                Top Consultants
               </h2>
               <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                Discover the best of the best in our keyboard selection.
+                Discover the best of the best. Our top consultants are ready to
+                help you with your business needs.
               </p>
             </div>
           </div>
           <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
             <Card>
               <CardHeader>
-                <h3 className="text-lg font-bold">Gaming Keyboard</h3>
+                <h3 className="text-lg font-bold">John Doe</h3>
               </CardHeader>
               <CardContent>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
-                  RGB lighting, fast response time, and customizable keys.
+                  Get help with your business strategy from a top consultant.
                 </p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader>
-                <h3 className="text-lg font-bold">Mechanical Keyboard</h3>
+                <h3 className="text-lg font-bold">Eliot</h3>
               </CardHeader>
               <CardContent>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
-                  Enjoy a satisfying typing experience with high-quality
-                  switches.
+                  Get help with your product design from a top consultant.
                 </p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader>
-                <h3 className="text-lg font-bold">Wireless Keyboard</h3>
+                <h3 className="text-lg font-bold">Macmillan</h3>
               </CardHeader>
               <CardContent>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
-                  Experience the freedom of wireless technology.
+                  Get help with your marketing strategy from a top consultant.
                 </p>
               </CardContent>
             </Card>
@@ -127,7 +127,7 @@ export default function ExploreExperts() {
               className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
               href="#"
             >
-              View All Products
+              View All Experts
             </Link>
           </div>
         </div>
